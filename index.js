b335419e04ba73882be92e8d9a91443c3640b1c6
Sam O'Regan <hurricane@hurricanedancer.com>
2021-05-03 09:24:32 +0200

    yeet

---
+++ index.js
@@ -0,0 +1,34 @@
+const rpc = require("discord-rpc");
+const client = new rpc.Client({ transport: "ipc" });
+
+client.on("ready", () => {
+  client.setActivity({
+    state: "An RPG game on Discord",
+    largeImageKey: "monsterhunt",
+    largeImageText: "Monsterhunt",
+    buttons: [
+      {
+        label: "Wiki",
+        url: "https://wiki.monsterhunt.xyz/en/home",
+      },
+      {
+        label: "Invite Bot",
+        url: "https://www.monsterhunt.xyz/invite",
+      },
+    ],
+  });
+
+  console.log("Presence is ready");
+});
+
+// initial login, may fail when discord first starts 
+client.login({
+  clientId: "836934112744701982",
+});
+
+// backup login if it does fail 
+setTimeout(() => {
+  client.login({
+    clientId: "836934112744701982",
+  });
+}, 120000);
